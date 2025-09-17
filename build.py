3df1c881ca1dc20399f0e0629c5cfa3099075a2c
atomix <maciejektomaszek10@gmail.com>
2025-09-17 15:36:46 +0200

    linux build.py filenotfounderror fix 2

--- build.py
+++ build.py
@@ -4,29 +4,29 @@ import json
 import subprocess
 
 def installdeps():
-    print("* Setting up virtual environment...")
+    print("Setting up virtual environment...")
     subprocess.run([sys.executable,"-m","venv","venv"],text=True,check=True)
     if sys.platform=="win32":
         venv_python = os.path.join("venv","Scripts","python.exe")
     else:
         venv_python = "./"+os.path.join("venv","bin","python")
-    print("* Installing colorama and PyInstaller modules...")
+    print("Installing colorama and PyInstaller modules...")
     subprocess.run([venv_python,"-m","pip","install","colorama","pyinstaller"],text=True,check=True)
 
 def build():
-    print("* Building executable...")
+    print("Building executable...")
     if sys.platform=="win32":
         subprocess.run([sys.executable,"-m","PyInstaller","-F","src/termadventure.py"],text=True,check=True)
     elif sys.platform=="linux":
-        subprocess.run([".venv/bin/python","-m","PyInstaller","-F","src/termadventure.py"],text=True,check=True)
-    print("* Cleaning up...")
+        subprocess.run(["./venv/bin/python","-m","PyInstaller","-F","src/termadventure.py"],text=True,check=True)
+    print("Cleaning up...")
     if sys.platform=="win32":
         subprocess.run(["powershell.exe", "-Command", "Remove-Item","-Recurse","build"],text=True,check=True)
         subprocess.run(["powershell.exe", "-Command", "Remove-Item","termadventure.spec"],text=True,check=True)
     elif sys.platform=="linux":
         subprocess.run(["rm","-rf","build"],text=True,check=True)
         subprocess.run(["rm","termadventure.spec"],text=True,check=True)
-    print("* Creating json scoreboard...")
+    print("Creating json scoreboard...")
     os.mkdir("data")
     filepath = os.path.join("data", "scoreboard.json")
     jsondata=[]
@@ -36,5 +36,5 @@ def build():
 if __name__ == "__main__":
     installdeps()
     build()
-    print("* Setup finished")
+    print("Setup finished")
     
